4da0e9da5241e3aca74c2bb93f9b508a6e2825d0
arch <miniyu157@163.com>
2026-02-08 16:04:52 +0800

    docs(preview): 📝 update demo.gif and build-gif.sh

--- demo/build-gif.sh
+++ demo/build-gif.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+SELF_PATH=$(realpath "${BASH_SOURCE[0]}")
+SELF_DIR="${SELF_PATH%/*}" SELF_DIR="${SELF_DIR:-/}"
+
+cp "$SELF_DIR/demo.sh" "/workdir"
+
 cd /workdir
+./demo.sh
 
 ./demo.sh --list | while IFS= read -r line; do
     rm -rf "./$line"
@@ -9,12 +15,20 @@ done
 
 vhs << 'EOF'
 Output demo.gif
-Set FontSize 22
+Set FontSize 24
 Set FontFamily "IosevkaTermSlab NF Obl"
 Set Width 1000
-Set Height 600
+Set Height 800
 Set Padding 10
 Set TypingSpeed 50ms
+
+Hide
+Type 'export PS1="\n\[\e[1;34m\]➜ \[\e[32m\]"'
+Enter
+Type 'export PS0="\e[0m"'
+Enter
+Ctrl+L
+Show
 
 Set TypingSpeed 30ms
 Type "rg '__greet' -A 1 -B 10 demo.sh | bat -pP --language=bash"
@@ -33,6 +47,8 @@ Sleep 0.5s
 Enter
 Sleep 1.5s
 
+Ctrl+L
+
 Type "./demo.sh --help"
 Sleep 0.5s
 Enter
@@ -48,18 +64,14 @@ Sleep 0.5s
 Enter
 Sleep 0.5s
 
-Set TypingSpeed 30ms
-Type "fd -t l -x ls -l {}"
-Set TypingSpeed 50ms
-Sleep 0.5s
-Enter
-
 Type "./greet '<Welcome to MonoBash!>'"
 Sleep 0.5s
 Enter
-Sleep 2.5s
+Sleep 3s
 EOF
 
 ./demo.sh --list | while IFS= read -r line; do
     rm -rf "./$line"
 done
+
+cp "./demo.gif" "$SELF_DIR/demo.gif"
