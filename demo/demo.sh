847dae43507c4ef6570deda573b9466cfb0190a6
arch <miniyu157@163.com>
2026-02-10 00:43:14 +0800

    chore(demo): 🔥 demo.sh: extract URL into variable

--- demo/demo.sh
+++ demo/demo.sh
@@ -4,10 +4,12 @@
 
 set -e
 
+URL_BASE="https://raw.githubusercontent.com/miniyu157/monobash/main"
+
 [[ -z ${BASH_SOURCE[0]} ]] && {
     md=$(
-        curl -fsSL "https://raw.githubusercontent.com/miniyu157/monobash/main/README.md" |
-            grep -n "curl -O https://raw.githubusercontent.com/miniyu157/monobash/main/demo/demo.sh" -C 5
+        curl -fsSL "$URL_BASE/README.md" |
+            grep -n "curl -O $URL_BASE/demo/demo.sh" -C 5
     )
     cat << EOF
 Why so lazy? Come on 😅
@@ -23,10 +25,10 @@ SELF="${SELF_PATH##*/}"
 SELF_DIR="${SELF_PATH%/*}" SELF_DIR="${SELF_DIR:-/}"
 
 printf "Fetching monobash from github.com/miniyu157/monobash ...\n"
-core=$(curl -fsSL "https://raw.githubusercontent.com/miniyu157/monobash/main/monobash")
+core=$(curl -fsSL "$URL_BASE/monobash")
 
 printf "Fetching demo/demo.patch from github.com/miniyu157/monobash ...\n"
-patch=$(curl -fsSL "https://raw.githubusercontent.com/miniyu157/monobash/main/demo/demo.patch.sh")
+patch=$(curl -fsSL "$URL_BASE/demo/demo.patch.sh")
 
 last_line=$(tail -n 1 <<< "$core")
 tmp_file="$SELF_DIR/_tmp_demo_$RANDOM.sh"
