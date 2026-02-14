87bc7d98a00e3e63c92deff42b137be17721efd8
arch <miniyu157@163.com>
2026-02-14 18:50:35 +0800

    docs(preview): 📝 update demo.gif and build-gif.sh

--- demo/build-gif.sh
+++ demo/build-gif.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+export all_proxy=http://127.0.0.1:7890
+export PATH=/workdir:$PATH
+
 SELF_PATH=$(realpath "${BASH_SOURCE[0]}")
 SELF_DIR="${SELF_PATH%/*}" SELF_DIR="${SELF_DIR:-/}"
 
@@ -15,11 +18,10 @@ done
 
 vhs << 'EOF'
 Output demo.gif
-Set Theme "Catppuccin Mocha"
 Set FontSize 24
 Set FontFamily "IosevkaTermSlab NF Obl"
 Set Width 1000
-Set Height 800
+Set Height 500
 Set Padding 10
 Set TypingSpeed 50ms
 
@@ -31,44 +33,47 @@ Enter
 Ctrl+L
 Show
 
+
 Set TypingSpeed 30ms
-Type "rg '__greet' -A 1 -B 10 demo.sh | bat -pP --language=bash"
+Type "head -n 7 demo.sh | bat -pP --language=bash"
 Set TypingSpeed 50ms
 Sleep 0.5s
 Enter
-Sleep 3s
+Sleep 2.5s
 
-Type "./demo.sh greet --help"
+Set TypingSpeed 30ms
+Type "rg '__greet' -A 1 -B 8 demo.sh | bat -pP --language=bash"
+Set TypingSpeed 50ms
+Sleep 0.5s
+Enter
+Sleep 2.5s
+
+
+Type "demo.sh greet --help"
 Sleep 0.5s
 Enter
 Sleep 3s
 
-Type "./demo.sh greet DreamFlower"
-Sleep 0.5s
-Enter
-Sleep 1.5s
-
-Ctrl+L
-
-Type "./demo.sh --help"
-Sleep 0.5s
-Enter
-Sleep 1.5s
-
-Type "./demo.sh --list"
-Sleep 0.5s
-Enter
-Sleep 1.5s
-
-Type "./demo.sh -L"
-Sleep 0.5s
-Enter
-Sleep 0.5s
-
-Type "./info"
+Type "demo.sh"
 Sleep 0.5s
 Enter
 Sleep 3s
+
+Type "demo.sh greet DreamFlower"
+Sleep 0.5s
+Enter
+Sleep 1.5s
+
+Type "ln -sf ./demo.sh greet"
+Sleep 0.5s
+Enter
+Sleep 1s
+
+Type "greet 'Welcome to MonoBash'"
+Sleep 0.5s
+Enter
+Sleep 4s
+
 EOF
 
 ./demo.sh --list | while IFS= read -r line; do
