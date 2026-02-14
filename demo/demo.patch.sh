c70f657b43dd1521a30339c3917023cbd948aacf
arch <miniyu157@163.com>
2026-02-14 18:42:36 +0800

    chore(demo): 🔥 update demo.patch.sh

--- demo/demo.patch.sh
+++ demo/demo.patch.sh
@@ -2,8 +2,6 @@ APP_DESC="A demonstration for the MonoBash framework."
 
 # @ui RED=31m     GREEN=32m     YELLOW=33m     BLUE=34m     MAGENTA=35m     CYAN=36m     WHITE=37m
 # @ui bgRED=41m   bgGREEN=42m   bgYELLOW=43m   bgBLUE=44m   bgMAGENTA=45m   bgCYAN=46m   bgWHITE=47m
-# @ui LRED=91m    LGREEN=92m    LYELLOW=93m    LBLUE=94m    LMAGENTA=95m    LCYAN=96m    LWHITE=37m
-# @ui bgLRED=101m bgLGREEN=102m bgLYELLOW=103m bgLBLUE=104m bgLMAGENTA=105m bgLCYAN=106m bgLWHITE=107m
 # @ui FOFF=39m BOFF=49m
 
 ## ${CYAN}Say hello to someone in the ${INVERT}terminal.${COFF}
@@ -24,11 +22,11 @@ __greet() {
 __check() {
     printf "${BOLD}Starting system diagnostics...${COFF}\n"
 
-    printf "  [${GRAY}....${COFF}] Kernel module"
+    printf "  [${FAINT}....${COFF}] Kernel module"
     sleep 0.3
     printf "\r  [${GREEN} OK ${COFF}] Kernel module\n"
 
-    printf "  [${GRAY}....${COFF}] Socket stream"
+    printf "  [${FAINT}....${COFF}] Socket stream"
     sleep 0.4
     printf "\r  [${GREEN} OK ${COFF}] Socket stream (12ms)\n"
 
